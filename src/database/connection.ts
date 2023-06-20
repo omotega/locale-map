4e5cacc234c035c3154e5b726167a08311f8d5df
omotega <tomoyibo@gmail.com>
2023-06-20 15:31:04 +0100

    faet:added database connection

---
+++ src/database/connection.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+import config from '../config/env';
+import logger from '../core/logger';
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(config.MONG0_URI);
+    console.log(`db connected at ${conn.connection.host}`);
+  } catch (error) {
+    logger.debug(error);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
