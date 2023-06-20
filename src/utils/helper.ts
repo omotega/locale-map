7e91ec55bc07159139db1a6a8b93af9504a30095
omotega <tomoyibo@gmail.com>
2023-06-20 15:25:29 +0100

    helpers for application

---
+++ src/utils/helper.ts
@@ -0,0 +1,34 @@
+import argon from 'argon2';
+import { v4 } from 'uuid';
+import crypto from 'crypto';
+
+async function hashPassword(password: string): Promise<string> {
+  const hashedPassword = await argon.hash(password);
+  return hashedPassword;
+}
+
+async function comparePassword(
+  hash: string,
+  password: string
+): Promise<boolean> {
+  const isMatch = await argon.verify(hash, password);
+  return isMatch;
+}
+
+async function genApiKey(): Promise<string> {
+  const value = v4();
+  const apikey = `locale_${value}`;
+  return apikey;
+}
+
+async function genKey(): Promise<string> {
+  const key = crypto.randomBytes(16).toString('hex');
+  return key;
+}
+
+export default {
+  hashPassword,
+  comparePassword,
+  genApiKey,
+  genKey
+};
