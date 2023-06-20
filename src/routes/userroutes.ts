7a202f85cdaece9c105731dd3e0dc428afb3e4ae
omotega <tomoyibo@gmail.com>
2023-06-20 15:32:45 +0100

    feat:added user route to application'

---
+++ src/routes/userroutes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+const userRouter = Router();
+
+import usercontroller from '../controller/usercontroller';
+import signUpValidationMiddleware from '../middleawre/validate';
+
+userRouter
+  .route('/signup')
+  .post(signUpValidationMiddleware, usercontroller.signup);
+userRouter.route('/login').post(usercontroller.login);
+
+export default userRouter;
