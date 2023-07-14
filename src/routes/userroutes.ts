d0b67c3b3ab76bf514dfa2ea1b3763d7fe534e3c
omotega <tomoyibo@gmail.com>
2023-07-14 02:47:29 +0100

    feat:added validation to user endpoits

--- src/routes/userroutes.ts
+++ src/routes/userroutes.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 const userRouter = Router();
 
 import usercontroller from '../controllers/usercontroller';
-import signUpValidationMiddleware from '../middleawre/validate';
+import validation from '../middleawre/validate';
 
 userRouter
   .route('/signup')
-  .post(signUpValidationMiddleware, usercontroller.signup);
-userRouter.route('/login').post(usercontroller.login);
+  .post(validation.signUpValidationMiddleware, usercontroller.signup);
+userRouter
+  .route('/login')
+  .post(validation.loginValidationMiddleware, usercontroller.login);
 
 export default userRouter;
